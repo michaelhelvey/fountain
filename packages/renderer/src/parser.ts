a080bf79ac490c96d96d98a8f5c377ec5b144908
Michael Helvey <michael.helvey1@gmail.com>
2019-03-16 07:54:32 -0700

    fix: cli bin name + build scripts

--- packages/renderer/src/parser.ts
+++ packages/renderer/src/parser.ts
@@ -7,11 +7,13 @@ import { FNTElement } from './element'
  * @param input String of fountain-compliant input text
  */
 export const parseToAST = (input: string): string => {
-  let output = ''
   // create array of lines
   const lines = input.split(/\n/).map(t => new Line(t))
 
   // iterate over the lines and break them up by element
+  const elements = lines.map(line => {
+    return new FNTElement([line])
+  })
 
-  return output
+  return JSON.stringify({ elements })
 }
